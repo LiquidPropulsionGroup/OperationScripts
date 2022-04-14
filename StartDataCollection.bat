8ea5ccf886e3d20c325bde7e19624546e3904a74
Thomas W. C. Carlson <72635603+TWCCarlson@users.noreply.github.com>
2022-04-13 21:46:31 -0700

    Start/Stop data collection/socketing

--- StartDataCollection.bat
+++ StartDataCollection.bat
@@ -1,13 +1,20 @@
+:: Control what gets printed
 echo off
 cls
 
+:: Change working directory 
 echo Starting Data Websocket . . .
 cd /D ../STINGER-OP-Deploy/ENGINE-SOCKET
+set location= %cd%
+::echo %PATH%
 
-set PATH= %cd%
-echo %PATH%
+:: Start up docker-compose
+cd %location% & start "LPG-Docker" dockerStart.bat
 
+:: Start the python virtual environment and install requirements
 python -m venv .venv
 call .venv\scripts\activate && pip install -r requirements.txt
-cd %PATH% & start "LPG-WebSocket" UIDataWebSocket.py
+
+:: Start the websocket .py code
+cd %location% & start "LPG-WebSocket" UIDataWebSocket.py
 
